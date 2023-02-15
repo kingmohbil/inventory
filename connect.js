b911e7876de7b0134e00e342408875bd45224831
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-02-15 18:26:34 +0300

    A couple of log's added to the function to display the current state of connection

--- connect.js
+++ connect.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+// a function to connect to the database that we are requiring in our application
 module.exports = async (connectionString) => {
   try {
+    console.log('connecting to the database...');
     await mongoose.connect(connectionString);
-    console.log('connected to database successfully...');
+    console.log('connected to database successfully');
   } catch (err) {
     console.log('failed to connect to database...');
     console.error(err.message);
