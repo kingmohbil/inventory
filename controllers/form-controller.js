edfd57bce72a3f77d896502b36f5ef54d15e02b8
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-02-15 18:29:39 +0300

    The express-validator middleware is added for validation and sanitizing

--- controllers/form-controller.js
+++ controllers/form-controller.js
@@ -1,80 +1,160 @@
-const mongoose = require('mongoose');
-const itemModel = require('../models/items-model');
-const categoryModel = require('../models/categories-model');
+const mongoose = require('mongoose'); // requiring mongoose for CRUD operations
+const { body, validationResult } = require('express-validator'); // requiring express validator
+const itemModel = require('../models/items-model'); // requiring our itemModel to create items
+const categoryModel = require('../models/categories-model'); // requiring our categoryModel to create categories
+const itemsModel = require('../models/items-model');
+// adding an item
+exports.addItem = [
+  body('name', '* Name can`t be empty')
+    .trim()
+    .isLength({ min: 1, max: 80 })
+    .escape(),
+  body('name').custom(async (value) => {
+    const exist = await itemModel.exists({ name: value });
+    if (exist) throw new Error('* Name already exists');
+    return true;
+  }),
+  body('description', '* Description shold at least contain 10 characters.')
+    .trim()
+    .isLength({ min: 10, max: 500 })
+    .escape(),
+  body('price', '* Price must be at least $0.1.').isFloat({ min: 0.1 }),
+  body('quantity', '* Qunatity must be an integer greater than 0.').isInt({
+    min: 0,
+  }),
+  async (req, res) => {
+    const errors = [];
+    const isValid = validationResult(req);
+    if (!isValid.isEmpty()) {
+      let result, itemsNum;
+      try {
+        result = await categoryModel.find({}, 'name');
+        itemsNum = await itemsModel.estimatedDocumentCount({});
+      } catch (err) {
+        errors.push({
+          msg: err.message,
+        });
+      } finally {
+        res.render('item-form', {
+          categories: result,
+          no_of_items: itemsNum,
+          errors: isValid.array(),
+          name: req.body.name,
+          description: req.body.description,
+          price: req.body.price,
+          quantity: req.body.quantity,
+        });
+      }
+    } else {
+      const category = await categoryModel.findOne(
+        { name: req.body.category },
+        '_id'
+      );
+      itemModel.create({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        number_in_stock: req.body.quantity,
+        category: category._id,
+        url: `/${req.body.category}/${req.body.name}`,
+      });
+      res.redirect('/');
+    }
+  },
+];
+// adding a category to the database
+exports.addCategory = [
+  body('name', '* Name cannot be empty').trim().isLength({ min: 1 }).escape(),
+  body('description', '* Description must be at least 10 characters')
+    .trim()
+    .isLength({ min: 10 })
+    .escape(),
+  body('name').custom(async (value) => {
+    const exist = await categoryModel.exists({ name: value });
+    if (exist)
+      throw new Error('* The name already exists please choose another one');
+    return true;
+  }),
+  body('name').custom(async (value) => {
+    const categoriesNumber = await categoryModel.estimatedDocumentCount({});
+    if (categoriesNumber > 21)
+      throw new Error('* The maximum number of categories is 20');
+    return true;
+  }),
+  async (req, res) => {
+    const errors = [];
+    const isValid = validationResult(req);
+    if (!isValid.isEmpty()) {
+      const categoriesNumber = await categoryModel.estimatedDocumentCount({});
+      res.render('category-form', {
+        no_of_categories: categoriesNumber,
+        errors: isValid.array(),
+        name: req.body.name,
+        description: req.body.description,
+      });
+    } else {
+      try {
+        categoryModel.create({
+          name: req.body.name,
+          description: req.body.description,
+          url: `/categories/${req.body.name}`,
+        });
+        res.redirect('/');
+      } catch (e) {
+        errors.push({
+          msg: e.message,
+        });
+        const categoriesNumber = await categoryModel.estimatedDocumentCount({});
+        res.render('category-form', {
+          no_of_categories: categoriesNumber,
+          errors: errors,
+          name: req.body.name,
+          description: req.body.description,
+        });
+      }
+    }
+  },
+];
 
-exports.addItem = async (req, res) => {
-  try {
-    const categoryId = await categoryModel.findOne(
-      { name: req.body.category },
-      '_id'
-    );
-    const exist = await itemModel.exists({ name: req.body.name });
-    if (exist) throw new Error('cannot insert duplicate items...');
-
-    const item = new itemModel({
-      name: req.body.name,
-      description: req.body.description,
-      price: req.body.price,
-      category: categoryId._id,
-      number_in_stock: req.body.quantity,
-      url: `/categories/${req.body.category}/`,
-    });
-
-    item.url += item._id;
-    await item.save();
-    console.log('item saved successfully....');
-  } catch (error) {
-    console.error(error.message);
-  } finally {
-    res.redirect('/');
-  }
-};
-
-exports.addCategory = async (req, res) => {
-  try {
-    const number_of_categories = await categoryModel.estimatedDocumentCount({});
-    const exist = await categoryModel.exists({ name: req.body.name });
-    if (number_of_categories > 21)
-      throw new Error('Max number of categories is 20...');
-    if (exist) throw new Error('The category already exists...');
-    const category = new categoryModel({
-      name: req.body.name,
-      description: req.body.description,
-      url: `/${req.body.name}/`,
-    });
-    category.url += category._id;
-    await category.save();
-    console.log('category saved successfully...');
-  } catch (err) {
-    console.error(err.message);
-  } finally {
-    res.redirect('/');
-  }
-};
-
+// rendering the item form
 exports.onItemFormLoad = async (req, res) => {
+  let result, itemsNum;
+  let errors = [];
   try {
-    if (mongoose.connection.readyState === 0)
-      throw new Error('the database is disconnected....');
-    const result = await categoryModel.find({}, 'name').orFail();
-    const itemsNum = await itemModel.estimatedDocumentCount({});
+    result = await categoryModel.find({}, 'name');
+    itemsNum = await itemModel.estimatedDocumentCount({});
     res.render('item-form', {
       categories: result,
       no_of_items: itemsNum,
+      errors: [],
     });
   } catch (e) {
-    console.log(e.message);
-    res.render('item-form');
+    errors.push({
+      msg: e.message,
+    });
+    res.render('item-form', {
+      categories: result,
+      no_of_items: itemsNum,
+      errors: errors,
+    });
   }
 };
-
-exports.number_of_categories = async (req, res) => {
-  let result;
+//rendering the categories form
+exports.renderCategoryForm = async (req, res) => {
+  let errors = [];
   try {
-    result = await categoryModel.estimatedDocumentCount({});
+    const result = await categoryModel.estimatedDocumentCount({});
+    res.render('category-form', {
+      no_of_categories: result,
+      errors: [],
+    });
   } catch (e) {
-    console.error(e.message);
-  } finally {
-    res.render('category-form', { no_of_categories: result });
+    errors.push({
+      msg: e.message,
+    });
+    res.render('category-form', {
+      no_of_categories: result,
+      errors: errors,
+    });
   }
 };
--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,39 +1,99 @@
-const itemModel = require('../models/items-model');
-const categoryModel = require('../models/categories-model');
-
-exports.onLoad = async (req, res) => {
-  let num_of_categories;
-  let num_of_items;
+const itemModel = require('../models/items-model'); // requiring the item model for the CRUD operations
+const categoryModel = require('../models/categories-model'); // require the category model for the CRUD operations
+// rendering the home page function
+exports.renderHome = async (req, res) => {
+  let num_of_categories = 0;
+  let num_of_items = 0;
   try {
-    num_of_categories = await categoryModel.estimatedDocumentCount({});
-    num_of_items = await itemModel.estimatedDocumentCount({});
+    num_of_categories = await categoryModel.estimatedDocumentCount({}); // awaiting for the number of categories in the database
+    num_of_items = await itemModel.estimatedDocumentCount({}); // awaiting for the number of items in the database
   } catch (err) {
-    console.log(err);
+    console.log(err); //catching any errors
+  } finally {
+    //rendering the home page and emmbedding the values
+    res.render('home', {
+      no_of_categories: num_of_categories,
+      no_of_items: num_of_items,
+    });
   }
-  res.render('home', {
-    no_of_categories: num_of_categories,
-    no_of_items: num_of_items,
-  });
 };
 
-exports.loadCategories = async (req, res) => {
-  let results;
+exports.renderCategories = async (req, res) => {
+  let results = [];
   try {
-    results = await categoryModel.find({});
+    results = await categoryModel.find({}); // awaiting for all of the categories
   } catch (e) {
-    console.error(e.message);
+    console.error(e.message); // logging the error if any
   } finally {
+    //rendering the categories page with the results
     res.render('categories', { categories: results });
   }
 };
-
+// rendering the items page with all of the items
+exports.renderAllItems = async (req, res) => {
+  try {
+    const result = await itemModel.find({}).populate('category'); // awaiting for all of the items and populating the category
+    // to get the name of the category
+    console.log(result); // logging the results
+    //rendering the items page with the results
+    res.render('items', { title: 'All Items', items: result });
+  } catch (e) {
+    //handling the errord and rendering the pafe with an empty array
+    console.error(e.message);
+    res.render('items', { title: 'All Items', items: [] });
+  }
+};
+// rendering a specific item from a category
+exports.renderItemsFromCategory = async (req, res) => {
+  try {
+    const results = await itemModel
+      .find({ category: req.params.id })
+      .populate('category')
+      .orFail(); //awaiting for the results and populating the category field and throwing an error if not found
+    console.log(results);
+    // reendering the items page, embedding the results and the title
+    res.render('items', { title: results[0].category.name, items: results });
+  } catch (e) {
+    //handeling the errors and redirecting to the home page
+    console.error(e.message);
+    res.redirect('/');
+  }
+};
+// deleting a category by it's id
 exports.deleteCategory = async (req, res) => {
   try {
-    const result = await categoryModel.deleteOne({ _id: req.params });
-    console.log('item was deleted successfully...');
-    res.redirect('/');
+    //awaiting for the delete and if there is an error it will be handeled
+    const result = await categoryModel.deleteOne({ _id: req.params.id });
+    // deleting all the items in that category
+    const deletedItems = await itemModel.delete({ category: req.params.id });
+    // checking for the deletion success
+    if (!result.acknowledged)
+      throw new Error(`Cannot delete category with id ${req.params.id}`);
+    console.log(
+      `category with id: ${req.params.id} was deleted successfully...`
+    );
   } catch (e) {
+    //handling the error and logging the message
     console.error(e.message);
-    res.end();
+  } finally {
+    //handling the error and logging the message
+    res.redirect('/');
+  }
+};
+// deleting a item by it's id
+exports.deleteItem = async (req, res) => {
+  try {
+    //awaiting for the delete and if there is an error it will be handeled
+    const result = await itemModel.deleteOne({ _id: req.params.id });
+    // checking for the deletion success
+    if (!result.acknowledged)
+      throw new Error(`Cannot delete item with id ${req.params.id}`);
+    console.log(`item with id ${req.params.id} was deleted successfully`);
+  } catch (e) {
+    //handling the error and logging the message
+    console.error(e.message);
+  } finally {
+    //handling the error and logging the message
+    res.redirect('/');
   }
 };
